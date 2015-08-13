9e9785ee22109f2dbef804d640e449576d5b4448
danielmills <danielmillst@gmail.com>
2015-08-13 18:14:23 -0400

    Initial Push to GitHub

---
+++ src/com/ulticraft/friedenterra/FriedenTerra.java
@@ -0,0 +1,18 @@
+package com.ulticraft.friedenterra;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FriedenTerra extends JavaPlugin
+{
+	@Override
+	public void onEnable()
+	{
+		
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		
+	}
+}
